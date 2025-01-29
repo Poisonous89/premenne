cc188b03e3a00a199b4ec71b9ed451e434a90f08
skutas23 <skutas23@Z00P1-04>
2025-01-29 13:50:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$o = 10;
+$s = " ahoj <br>";
+$e = 5;
+//echo ("obsah obdlznika je ".$o*$e." m<sup>2</sup>.");
+#ahoj
+//ahoj
+/*
+cau
+*/
+/**
+ * 
+ */
+$a = 2;
+$b = 10;
+$c = 100;
+if  ($a>$b){
+    echo($a);
+}
+else if ($b>$c){
+    echo($b);
+}
+else{
+    echo($c);
+}
+
+
+
+
+
+
+?>
