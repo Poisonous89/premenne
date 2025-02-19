360b148a8f756a950300eecf5559501644582ef1
Poisonous89 <129315440+Poisonous89@users.noreply.github.com>
2025-02-19 08:14:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,29 @@ else{
 }
 
 
+$listZaciatok = [12,4,3,2,10,6,5,4,3,9]; $velkostListu = count($listZaciatok);
 
+$listVysledok = [];
+
+for ($j=0; $j < $velkostListu; $j++) {
+
+$najmensie = $listZaciatok[0];
+
+for ($i=0; $i < count($listZaciatok); $i++) {
+
+if ($najmensie > $listZaciatok[$i]) {
+
+$najmensie = $listZaciatok[$i];
+
+Sporadie = $i;
+
+}
+
+} echo("Najmensie v cykle: ". $najmensie. "< br >"); $listVysledok[$j] = $najmensie; array_splice($listZaciatok, $poradie, 1);
+
+for ($i=0; $i < count($listVysledok); $i++) { echo($listVysledok[$i].", ");
+
+}
 
 
 
